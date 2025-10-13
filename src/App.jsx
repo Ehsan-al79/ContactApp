5540e3192edfc171f9843ed310c88a8b8e8b4138
Ehsan <e.alimardani1379@gmail.com>
2025-10-13 19:45:11 +0330

    React Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from "react";
+
+import Form from "./components/project ContactApp/Form";
+import Header from "./components/project ContactApp/Header";
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="flex w-full flex-col justify-center items-center">
+      <Header/>
+      <Form/>
+    </div>
+    </>
+  );
+}
+
+export default App;
